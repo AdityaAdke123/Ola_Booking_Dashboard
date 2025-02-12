322413eb453f9675bf0dda2e0eed0b867054bb28
AdityaAdke123 <85841689+AdityaAdke123@users.noreply.github.com>
2025-02-12 15:49:29 -0500

    Update Ola-Ride-Data-Analysis-SQL.sql

--- Ola-Ride-Data-Analysis-SQL.sql
+++ Ola-Ride-Data-Analysis-SQL.sql
@@ -2,6 +2,7 @@
 USE ola;
 
 -- 1. Retrieve all successful bookings:
+
 CREATE VIEW SUCESSFUL_BOOKINGS AS
 SELECT * FROM bookings
 WHERE Booking_Status="Success";
@@ -9,6 +10,7 @@ WHERE Booking_Status="Success";
 SELECT * FROM SUCESSFUL_BOOKINGS;
 
 -- 2. Find the average ride distance for each vehicle type:
+
 CREATE VIEW AVERAGE_RIDE_DISTANCE AS
 SELECT Vehicle_Type,avg(Ride_Distance)
 as avg_distance from bookings group by Vehicle_Type;
@@ -16,6 +18,7 @@ as avg_distance from bookings group by Vehicle_Type;
 SELECT * FROM AVERAGE_RIDE_DISTANCE;
 
 -- 3. Get the total number of cancelled rides by customers:
+
 CREATE VIEW TOTAL_CANCELLED_RIDES_BY_CUSTOMERS AS 
 SELECT COUNT(*) FROM Bookings 
 WHERE Booking_Status="Canceled by Customer";
@@ -23,6 +26,7 @@ WHERE Booking_Status="Canceled by Customer";
 SELECT * FROM TOTAL_CANCELLED_RIDES_BY_CUSTOMERS;
 
 -- 4. List the top 5 customers who booked the highest number of rides:
+
 CREATE VIEW CUST_BOOKED_HIGHEST_RIDES AS
 SELECT Customer_ID, COUNT(Booking_ID) AS OCCURANCE_COUNT 
 FROM Bookings
@@ -33,6 +37,7 @@ LIMIT 5;
  SELECT * FROM CUST_BOOKED_HIGHEST_RIDES;
 
 -- 5. Get the number of rides cancelled by drivers due to personal and car-related issues:
+
 CREATE VIEW cancelled_by_drivers_P_C_issues AS
 SELECT COUNT(*) FROM Bookings
 WHERE Canceled_Rides_by_Driver="Personal & Car related issue";
@@ -40,6 +45,7 @@ WHERE Canceled_Rides_by_Driver="Personal & Car related issue";
 SELECT * FROM cancelled_by_drivers_P_C_issues;
 
 -- 6. Find the maximum and minimum driver ratings for Prime Sedan bookings:
+
 CREATE VIEW MAX_MIN_DRIVER_RATING AS
 SELECT max(Driver_Ratings) AS MAX_Prime_Sedan_Ratings, min(Driver_Ratings) AS MIN_Prime_Sedan_Ratings
 FROM Bookings
